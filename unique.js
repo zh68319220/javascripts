7208285d4476a0283fee117ca2b732bf03a8ae1c
zhang.yufeng <420866750@qq.com>
2017-02-28 15:33:22 +0800

    add notes

---
+++ unique.js
@@ -0,0 +1,28 @@
+// 数组去重
+
+Array.prototype.unique2 = function(){
+ this.sort(); //先排序
+ var res = [this[0]];
+ for(var i = 1; i < this.length; i++){
+  if(this[i] !== res[res.length - 1]){
+   res.push(this[i]);
+  }
+ }
+ return res;
+}
+var arr = [1, 'a', 'a', 'b', 'd', 'e', 'e', 1, 0]
+alert(arr.unique2());
+
+Array.prototype.unique3 = function(){
+ var res = [];
+ var json = {};
+ for(var i = 0; i < this.length; i++){
+  if(!json[this[i]]){
+   res.push(this[i]);
+   json[this[i]] = 1;
+  }
+ }
+ return res;
+}
+var arr = [112,112,34,'你好',112,112,34,'你好','str','str1'];
+alert(arr.unique3());
